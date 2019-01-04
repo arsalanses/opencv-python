222ee2d6bca10c958c40d05530fb458707e84336
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 18:53:17 +0330

    add logo

--- lesson8/lesson8.py
+++ lesson8/lesson8.py
@@ -2,8 +2,25 @@ import cv2
 import numpy as np
 
 img1 = cv2.imread('image.tif')
-img2 = cv2.imread('imglogo.tif')
+img2 = cv2.imread('imglogo.png')
 
 rows, cols, channels = img2.shape
 roi = img1[0:rows, 0:cols]
 
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+ret, mask = cv2.threshold(img2gray, 230, 255, cv2.THRESH_BINARY_INV)
+
+mask_inv = cv2.bitwise_not(mask)
+
+img1_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
+
+img2_fg = cv2.bitwise_and(img2, img2, mask = mask)
+
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+
+cv2.imshow('final', img1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
