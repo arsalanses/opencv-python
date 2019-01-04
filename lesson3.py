4dffd9a10fc3758665d1115585051529d3660c08
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 14:24:06 +0330

    show webcam

--- lesson3.py
+++ lesson3.py
@@ -3,3 +3,14 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('webCam', gray)
+    
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
