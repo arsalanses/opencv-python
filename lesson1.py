07cdcd6a8a1439dc5d5b9802dac2c154f17fd921
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 13:50:51 +0330

    import pyplot

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,12 @@
 import cv2
-import numpy as numpy
+import numpy as np
+from matplotlib import pyplot as plt
 
 img = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
-
 cv2.imshow('image', img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.show()
 
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
