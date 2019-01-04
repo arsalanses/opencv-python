73cd3b0706d50ec49a3a26f5136c46500a9b31ac
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 15:33:20 +0330

    draw circle and polylines

---
+++ lesson5.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2.line(frame, (100, 200), (200, 400), (0, 255, 0), 10)
+    # cv2.rectangle(frame, (100, 200), (200, 300), (0, 0, 255), 5)
+
+    cv2.circle(frame, (200, 200), 70, (120, 0, 50), 3)
+
+    pts = np.array([[50, 100], [120, 200], [250, 100], [400, 20]])
+    cv2.polylines(frame, [pts], True, (60, 40 , 20), 5)
+
+
+    cv2.imshow('webCam', frame)
+
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
