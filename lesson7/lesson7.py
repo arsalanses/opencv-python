976125f62e8cf8fd0230c4951c7e8b11d57868a0
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 17:49:38 +0330

    add img cv2

--- lesson7/lesson7.py
+++ lesson7/lesson7.py
@@ -4,4 +4,13 @@ import numpy as np
 img1 = cv2.imread('image1.tif')
 img2 = cv2.imread('image2.tif')
 
-add = img1 + img2
+# add = img1 + img2
+# add = cv2.add(img1, img2)
+add = cv2.addWeighted(img1, 0.4, img2, 0.7, 0)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('add', add)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
