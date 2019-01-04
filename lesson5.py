9f1a94dbd9a377cab9a6ee31748edcfe9012d94b
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-04 15:37:06 +0330

    put text

--- lesson5.py
+++ lesson5.py
@@ -6,14 +6,18 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     # cv2.line(frame, (100, 200), (200, 400), (0, 255, 0), 10)
+    
     # cv2.rectangle(frame, (100, 200), (200, 300), (0, 0, 255), 5)
 
-    cv2.circle(frame, (200, 200), 70, (120, 0, 50), 3)
+    # cv2.circle(frame, (200, 200), 70, (120, 0, 50), 3)
 
-    pts = np.array([[50, 100], [120, 200], [250, 100], [400, 20]])
-    cv2.polylines(frame, [pts], True, (60, 40 , 20), 5)
+    # pts = np.array([[50, 100], [120, 200], [250, 100], [400, 20]])
+    # cv2.polylines(frame, [pts], True, (60, 40 , 20), 5)
 
+    font = cv2.FONT_HERSHEY_COMPLEX
+    cv2.putText(frame, 'arsalan', (100, 100), font, 1, (100, 0, 0), 5)
 
     cv2.imshow('webCam', frame)
 
