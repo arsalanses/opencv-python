36dd6da1bf106aaa1ea0eea265f7f1047caceb56
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-05 17:24:10 +0330

    show text functions

--- lesson9/lesson9.py
+++ lesson9/lesson9.py
@@ -2,8 +2,12 @@ import cv2
 import numpy as np
 
 img = cv2.imread('image.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, threshold = cv2.threshold(gray, 12, 255, cv2.THRESH_BINARY)
+th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 
-cv2.imshow('page', img)
+cv2.imshow('page', threshold)
+cv2.imshow('page2', th)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
