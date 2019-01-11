2ef56589559e44a7fea351cde513ef0320cb591e
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2019-01-11 17:52:48 +0330

    add 2 mask

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -7,10 +7,20 @@ while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 0, 0])
-    upper_red = np.array([255, 255, 255])
+    # lower mask (0-10)
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    mask0 = cv2.inRange(hsv, lower_red, upper_red)
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    # upper mask (170-180)
+    lower_red = np.array([170, 50, 50])
+    upper_red = np.array([180, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    # join my masks
+    mask = mask0 + mask1
+
+    mask = cv2.bitwise_not(mask)
     res = cv2.bitwise_and(frame, frame, mask = mask)
 
     cv2.imshow('frame', frame)
